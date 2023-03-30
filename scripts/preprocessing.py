f0b22a769c560a0ea826d5ecfeaa829db87717ff
seyedfarhadtorabi <102485348+seyedfarhadtorabi@users.noreply.github.com>
2023-03-30 15:58:17 +0430

    Update preprocessing.py

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,10 +1,26 @@
+import pandas as pd
 import re
 import string
+import matplotlib.pyplot as plt
+import seaborn as sns
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 import spacy
+nltk.download('punkt')
+nltk.download('stopwords')
+# Load data
+df = pd.read_csv('data/customer_complaints.csv')
 
+# Check the distribution of complaint text lengths
+df['text_length'] = df['Consumer complaint narrative'].str.len()
+sns.histplot(x='text_length', data=df, bins=50)
+plt.title('Distribution of Complaint Text Lengths')
+plt.show()
+# Remove missing values
+df.dropna(subset=['Consumer complaint narrative'], inplace=True)
+df=df.head(100)
+# Define functions for text cleaning and tokenization
 def clean_text(text):
     # Remove punctuation
     text = text.translate(str.maketrans('', '', string.punctuation))
@@ -21,3 +37,11 @@ def clean_text(text):
     doc = nlp(' '.join(filtered_words))
     lemmatized_words = [token.lemma_ for token in doc]
     return ' '.join(lemmatized_words)
+
+# Apply text cleaning and tokenization to complaint text
+df['clean_text'] = df['Consumer complaint narrative'].apply(clean_text)
+
+
+
+# Save cleaned data to file
+df.to_csv('data/complaints_clean.csv', index=False)
